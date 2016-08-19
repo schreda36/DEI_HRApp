ec2a1afa86cc8ffb904a7685327a11749ac839d1
Dan Schreck <schreda@yahoo.com>
2016-08-18 20:46:24 -0500

    updated ui.R

--- ui.R
+++ ui.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(markdown)
 
 shinyUI(navbarPage("Navbar!",
@@ -9,15 +8,15 @@ shinyUI(navbarPage("Navbar!",
                                                      c("Salary"="Salary", "Total Compensation"="TotalCompensation")
                                         ),
                                         checkboxGroupInput("criteria", "Data Criteria:",
-                                                    c("Job Title" = "JobTitle",
-                                                      "Years of School" = "YrsSchool",
-                                                      "Years of Experience" = "YrsExp",
-                                                      "Prior Performance" = "Performance",
-                                                      "Country" = "Country",
-                                                      "Location" = "Location",
-                                                      "Organization" = "Organization",
-                                                      "Manager" = "Manager"
-                                                      ),selected="JobTitle"),
+                                                           c("Job Title" = "JobTitle",
+                                                             "Years of School" = "YrsSchool",
+                                                             "Years of Experience" = "YrsExp",
+                                                             "Prior Performance" = "Performance",
+                                                             "Country" = "Country",
+                                                             "Location" = "Location",
+                                                             "Organization" = "Organization",
+                                                             "Manager" = "Manager"
+                                                           ),selected="JobTitle"),
                                         radioButtons("FactorType", "Factor:",
                                                      c("Gender"="Gender", "Ethnicity"="e")
                                         ),
@@ -35,12 +34,12 @@ shinyUI(navbarPage("Navbar!",
                                                     tabPanel("Summary", verbatimTextOutput("summary")), 
                                                     tabPanel("Plot", 
                                                              fluidRow(column(width = 12,
-                                                                   plotOutput("plot", click = "plot_click")
-                                                                   )),
-                                                             fluidRow(column(width = 5,
-                                                                   verbatimTextOutput("click_info")
-                                                                   ))
-                                                            )
+                                                                             plotOutput("plot", click = "plot_click", brush = "plot_brush")
+                                                             )),
+                                                             fluidRow(column(width = 12,
+                                                                             verbatimTextOutput("click_info")
+                                                             ))
+                                                    )
                                                     
                                         )
                                   )
@@ -72,72 +71,4 @@ shinyUI(navbarPage("Navbar!",
                    #                     )
                    #            )
                    # )
-=======
-library(markdown)
-
-shinyUI(navbarPage("Navbar!",
-                   tabPanel("Factor",
-                            sidebarLayout(
-                                  sidebarPanel(
-                                        radioButtons("DataType", "Data to Analyze:",
-                                                     c("Salary"="Salary", "Total Compensation"="TotalCompensation")
-                                        ),
-                                        checkboxGroupInput("criteria", "Data Criteria:",
-                                                    c("Title" = "JobTitle",
-                                                      "Years of School" = "YrsSchool",
-                                                      "Years of Experience" = "YrsExp",
-                                                      "Prior Performance" = "Performance",
-                                                      "Country" = "Country",
-                                                      "Location" = "Location",
-                                                      "Organization" = "Organization",
-                                                      "Manager" = "Manager"
-                                                      )),
-                                        radioButtons("FactorType", "Factor:",
-                                                     c("Gender"="Gender", "Ethnicity"="e")
-                                        ),
-                                        selectInput("variance", "Allowable Variance:",
-                                                    c("< 2.5%" = ".025",
-                                                      "< 5%" = ".05",
-                                                      "< 10%" = ".1"
-                                                    )),
-                                        actionButton("goButton", "Go!")
-                                  ),
-                                  mainPanel(
-                                        tabsetPanel(type = "tabs", 
-                                                    tabPanel("Table", dataTableOutput("table")),
-                                                    #tabPanel("Table", textOutput("table")),
-                                                    tabPanel("Summary", verbatimTextOutput("summary")), 
-                                                    tabPanel("Plot", plotOutput("plot"))
-                                                    
-                                        )
-                                  )
-                            )
-                   )
-                   # ,tabPanel("Summary",
-                   #          verbatimTextOutput("summary")
-                   # ),
-                   # navbarMenu("More",
-                   #            tabPanel("Table",
-                   #                     verbatimTextOutput("summary")
-                   #            ),
-                   #            tabPanel("About",
-                   #                     fluidRow(
-                   #                           column(6,
-                   #                                  verbatimTextOutput("summary")
-                   #                                  #includeMarkdown("about.md")
-                   #                           )
-                   #                           ,
-                   #                           column(3,
-                   #                                  img(class="img-polaroid",
-                   #                                      src=paste0("http://www.vayam-llc.com/wp-content/themes/Vayam%20LLC/images/vayam-logo-small.png")),
-                   #                                  tags$small(
-                   #                                        "Copyright 2016 ",
-                   #                                        "Vayam LLC ",
-                   #                                        a(href="http://www.vayam-llc.com")
-                   #                                  )
-                   #                           )
-                   #                     )
-                   #            )
-                   # )
->>>>>>> origin/master
 ))
