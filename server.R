24631feaf595029728b5c37e249f321c355d39e1
Dan Schreck <schreda@yahoo.com>
2016-08-18 20:47:01 -0500

    update server.R

--- server.R
+++ server.R
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
-
 suppressPackageStartupMessages(c(library(tm),library(shiny),library(dplyr),library(plyr),library(scales),library(stringr),library(ggplot2)))
 
 #source("global.R")
 setwd("C:/Users/schre/DataScience/wd/EEO/app")
 
 shinyServer(function(input, output) {
-
+      
       dT <- reactive ({
             if(input$DataType=="Salary")
                   "CurrentSalary"
@@ -17,42 +15,14 @@ shinyServer(function(input, output) {
       dataset <- reactive({
             ds <- read.csv("./data/SampleData.csv")
             #if(input$DataType=="Salary")
-             #     dataSubset <- subset(ds, select=c(Gender, CurrentSalary, JobTitle))
+            #     dataSubset <- subset(ds, select=c(Gender, CurrentSalary, JobTitle))
             #else if(input$DataType=="Total Compensation")
-             #     dataSubset <- subset(ds, select=c(Gender, TotalCompensation, JobTitle))
+            #     dataSubset <- subset(ds, select=c(Gender, TotalCompensation, JobTitle))
             dCols <- c("Gender", dT(), "JobTitle")
             dataSubset <- subset(ds, select=dCols)
             dataSubset
       })
-      
-      
-      # dss.factor.avg <- reactive({
-      #       ds <- dataset()
-      #       dss.factor.avg <- aggregate(CurrentSalary ~ Gender + JobTitle, data=ds, mean)
-      # })
-      
-      # basic gender, race averages by title
-      output$plotX <- renderPlot({
-            if (input$goButton > 0) {
-                  inFactor <- input$FactorType
-                  ds <- dataset()
-                 
-                  #mean by factor and critiera
-                  ds.factor.avg <- aggregate(CurrentSalary ~ Gender + JobTitle, data=dataset(), mean) 
 
-                  plot(pm2.5.data.balt$year, pm2.5.data.balt$Emissions, type="b", pch=23, bg="blue", xlab="Year", 
-                       ylab="Total Emissions (thousands of tons)", main="Baltimore PM2.5 Emissions")
-                  
-                  # p <- ggplot(dataSubset, aes(dataSubset$JobTitle, dataSubset$CurrentSalaryFTE, color=dataSubset$Gender)) 
-                  # p <- p + geom_point() 
-                  # p <- p + geom_point(data = dataSubset, aes(x=Gender,y=as.double(predSal())), color='red', size=4)
-                  # p <- p + labs(x="Job Title",y="Annual Salary (USD$)") + scale_y_continuous(labels=scales::dollar)
-                  # p <- p + theme(axis.text.x = element_text(angle = 45, hjust = 1))
-                  # p <- p + guides(color=guide_legend(title="Perf Rating"))
-                  # p
-            }
-      }, height=300)
-      
       processData <- reactive({
             ds <- dataset() #dataSubset
             
@@ -64,11 +34,12 @@ shinyServer(function(input, output) {
                   ds.factor.avg <- aggregate(CurrentSalary ~ Gender + JobTitle, data=ds, mean)
                   ds.factor.avg <- data.frame(ds.factor.avg)
                   
+                  ds.factor.M <- ddply(ds.factor.avg,.(JobTitle),
+                                       summarise,meanM = mean(CurrentSalary[Gender == "M"]))
+                  
                   ds.factor.F <- ddply(ds.factor.avg,.(JobTitle),
                                        summarise,meanF = mean(CurrentSalary[Gender == "F"]))
                   
-                  ds.factor.M <- ddply(ds.factor.avg,.(JobTitle),
-                                       summarise,meanM = mean(CurrentSalary[Gender == "M"]))
             }
             else if(input$DataType=="TotalCompensation") {
                   ds.factor.avg <- aggregate(TotalCompensation ~ Gender + JobTitle, data=ds, mean)
@@ -92,7 +63,7 @@ shinyServer(function(input, output) {
       
       output$table <- renderDataTable({
             if (input$goButton > 0) {
-
+                  
                   ds.factor.var <- processData()
                   ds.factor.var$difference <- percent(ds.factor.var$difference)
                   
@@ -109,114 +80,53 @@ shinyServer(function(input, output) {
       
       output$plot <- renderPlot({
             ds.factor.var <- processData() 
-            #hist(as.vector(ds.factor.var$difference))
-            p <- ggplot(ds.factor.var, aes(JobTitle, difference, size=6))
-            p <- p + geom_point()
-            p <- p + labs(title =" New title", x = "Job Titles", y = "% Difference") 
-            #p <- p + geom_text(aes(label = JobTitle))
-            #p <- qplot(ds.factor.var$JobTitle, ds.factor.var$difference, data=ds.factor.var)
-            p 
+            #ds.factor.var <- subset(ds.factor.var)
+            #ds.factor.var$difference <- percent(ds.factor.var$difference)
+            
+            # p <- ggplot(ds.factor.var, aes(JobTitle, difference))
+            # p <- p + geom_point()
+            # p <- p + labs(title =" New title", x = "Job Titles", y = "% Difference") 
+            
+            #plot_click appears to work better with basic plot, not ggplot2
+            p <- plot(x=ds.factor.var[,1], y=ds.factor.var$difference, xlab="Job Title", ylab="% Difference")
+            p
       })
       
       output$click_info <- renderPrint({
-            cat("input$plot_click:\n")
-            str(input$plot_click)
-      })
-=======
-
-suppressPackageStartupMessages(c(library(tm),library(shiny),library(dplyr),library(plyr),library(stringr),library(ggplot2)))
-
-#source("global.R")
-setwd("C:/Users/schre/DataScience/wd/EEO/app")
-
-shinyServer(function(input, output) {
-
-      dT <- reactive ({
-            if(input$DataType=="Salary")
-                  "CurrentSalary"
-            else if(input$DataType=="TotalCompensation")
-                  "TotalCompensation"
-      })
-      
-      dataset <- reactive({
-            ds <- read.csv("./data/SampleData.csv")
-            #if(input$DataType=="Salary")
-             #     dataSubset <- subset(ds, select=c(Gender, CurrentSalary, JobTitle))
-            #else if(input$DataType=="Total Compensation")
-             #     dataSubset <- subset(ds, select=c(Gender, TotalCompensation, JobTitle))
-            dCols <- c("Gender", dT(), "JobTitle")
-            dataSubset <- subset(ds, select=dCols)
-            dataSubset
-      })
-      
-      
-      # dss.factor.avg <- reactive({
-      #       ds <- dataset()
-      #       dss.factor.avg <- aggregate(CurrentSalary ~ Gender + JobTitle, data=ds, mean)
-      # })
-      
-      # basic gender, race averages by title
-      output$plotX <- renderPlot({
-            if (input$goButton > 0) {
-                  inFactor <- input$FactorType
-                  ds <- dataset()
-                 
-                  #mean by factor and critiera
-                  ds.factor.avg <- aggregate(CurrentSalary ~ Gender + JobTitle, data=dataset(), mean) 
-
-                  plot(pm2.5.data.balt$year, pm2.5.data.balt$Emissions, type="b", pch=23, bg="blue", xlab="Year", 
-                       ylab="Total Emissions (thousands of tons)", main="Baltimore PM2.5 Emissions")
-                  
-                  # p <- ggplot(dataSubset, aes(dataSubset$JobTitle, dataSubset$CurrentSalaryFTE, color=dataSubset$Gender)) 
-                  # p <- p + geom_point() 
-                  # p <- p + geom_point(data = dataSubset, aes(x=Gender,y=as.double(predSal())), color='red', size=4)
-                  # p <- p + labs(x="Job Title",y="Annual Salary (USD$)") + scale_y_continuous(labels=scales::dollar)
-                  # p <- p + theme(axis.text.x = element_text(angle = 45, hjust = 1))
-                  # p <- p + guides(color=guide_legend(title="Perf Rating"))
-                  # p
-            }
-      }, height=300)
-      
-      output$table <- renderDataTable({
-            if (input$goButton > 0) {
-                  dCols <- c("Gender", dT(), "JobTitle")
-                  ds <- dataset() #dataSubset
-                  #higher allowed variance should produce fewer results
-                  AllowedVariance <- as.numeric(input$variance)
-      
-                  #get means by Criteria for each Factor
-                  if(input$DataType=="Salary") {
-                        ds.factor.avg <- aggregate(CurrentSalary ~ Gender + JobTitle, data=ds, mean)
-                        ds.factor.avg <- data.frame(ds.factor.avg)
-                        
-                        ds.factor.F <- ddply(ds.factor.avg,.(JobTitle),
-                                             summarise,meanF = mean(CurrentSalary[Gender == "F"]))
-                        
-                        ds.factor.M <- ddply(ds.factor.avg,.(JobTitle),
-                                             summarise,meanM = mean(CurrentSalary[Gender == "M"]))
-                  }
-                  else if(input$DataType=="TotalCompensation") {
-                        ds.factor.avg <- aggregate(TotalCompensation ~ Gender + JobTitle, data=ds, mean)
-                        ds.factor.avg <- data.frame(ds.factor.avg)
-                        
-                        ds.factor.F <- ddply(ds.factor.avg,.(JobTitle),
-                                             summarise,meanF = mean(TotalCompensation[Gender == "F"]))
-                        
-                        ds.factor.M <- ddply(ds.factor.avg,.(JobTitle),
-                                             summarise,meanM = mean(TotalCompensation[Gender == "M"]))
-                  }
-                        
-                  
             
-                  ds.factor <- merge(ds.factor.F, ds.factor.M)
-      
-                  #get subset of data filter by allowed variance
-                  difference <- (ds.factor$meanM-ds.factor$meanF)/ds.factor$meanM
-                  ds.factor.var <- filter(ds.factor, difference>AllowedVariance)
-                  ds.factor.var
-            }
+            ds.factor.var <- data.frame(processData())
+            #ds.factor.var$difference <- percent(ds.factor.var$difference)
             
+            # if(input$DataType=="Salary") {
+            #       colnames(ds.factor.var) <- c("Job Title","Avg Salary (male)","Avg Salary (female)","% Difference")
+            # }
+            # else if(input$DataType=="TotalCompensation") {
+            #       colnames(ds.factor.var) <- c("Job Title","Avg TComp (male)","Avg TComp (female)","% Difference")
+            # }
+            
+            cat("Click and drag mouse over data-points for more information.\n")
+            # brush <- input$plot_brush
+            # plot.range <- reactiveValues(x = NULL, y = NULL)
+            # plot.range$x <- c(brush$xmin, brush$xmax)
+            
+            #ds.factor.var <- brushedPoints(ds.factor.var,input$plot_brush, xvar ="Job Title",yvar ="% Difference")
+            #ds.factor.var <- subset(ds.factor.var,select=c("Job Title","% Difference"))
+            
+            dpoint <- brushedPoints(ds.factor.var, input$plot_brush, xvar ="JobTitle",yvar ="difference")
+            #dpoint <- nearPoints(ds.factor.var, input$plot_click, xvar="Job Title",yvar="% Difference")
+            
+
+            
+            if (nrow(dpoint) == 0)
+                  return()
+            # else {
+                   # keeprows <- round(plot.range$x) == ds.factor.var["Job Title"]
+                   # head(ds.factor.var[keeprows, ], 10)
+            #       #as.numeric(ds.factor.var["Job Title"])
+            #       #plot.range$x
+            # }
+            dpoint
+            #ds.factor.var
+
       })
-      
->>>>>>> origin/master
 })
